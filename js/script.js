efd7b0ac6e6d7945d8fd48d2209d07da1fc04b1a
Christopher White <chrismw44@gmail.com>
2018-08-08 15:45:37 +0100

    Quotes added

--- js/script.js
+++ js/script.js
@@ -3,39 +3,39 @@
 // Create the array of quote objects and name it quotes
 var quotes = [
   {
-    quote: ,
-    source: ,
-    citation: ,
-    year: ,
-    tag:
+    quote: "Don't quit. Suffer now and live the rest of your life as a champion",
+    source: "Muhammad Ali",
+    citation: "Twitter",
+    year: "2013",
+    field: "Sport"
   },
   {
-    quote: ,
-    source: ,
+    quote: "The person who reads too much and uses his brain too little will fall into lazy habits of thinking",
+    source: "Albert Einstein",
     citation: ,
     year: ,
-    tag:
+    field: "Science"
   },
   {
-    quote: ,
-    source: ,
-    citation: ,
-    year: ,
-    tag:
+    quote: "It is our choices, that show what we truly are, far more than our abilities",
+    source: "J.K. Rowling",
+    citation: "Harry Potter and the Chamber of Secrets",
+    year: "1998",
+    field: "Literature"
   },
   {
-    quote: ,
-    source: ,
+    quote: "If a person has no dreams, they no longer have any reason to live. Dreaming is necessary, although in the dream reality should be glimpsed. For me this is the principle of life.",
+    source: "Ayrton Senna",
     citation: ,
     year: ,
-    tag:
+    field: "Sport"
   },
   {
-    quote: ,
-    source: ,
+    quote: "It seems to me that the natural world is the greatest source of excitement; the greatest source of visual beauty; the greatest source of intellectual interest. It is the greatest source of so much in life that makes life worth living",
+    source: "David Attenborough",
     citation: ,
-    year: ,
-    tag:
+    year: "2000",
+    field: "Nature"
   }
 ];
 
