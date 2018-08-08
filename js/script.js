6bb834f46231f5398e4e35ae0cf1c4188ec89d5d
Christopher White <chrismw44@gmail.com>
2018-08-08 19:53:37 +0100

    Extra property added

--- js/script.js
+++ js/script.js
@@ -3,33 +3,33 @@
 // Create the array of quote objects and name it quotes
 var quotes = [
   {
-    quote: '"Don\'t quit. Suffer now and live the rest of your life as a champion"',
-    source: "Muhammad Ali",
-    citation: "Twitter",
-    year: "2013",
-    field: "Sportsman"
+    quote: 'Don\'t quit. Suffer now and live the rest of your life as a champion',
+    source: 'Muhammad Ali',
+    citation: 'Twitter',
+    year: '2013',
+    field: '(Sportsman)'
   },
   {
-    quote: '"The person who reads too much and uses his brain too little will fall into lazy habits of thinking"',
-    source: "Albert Einstein",
-    field: "Scientist"
+    quote: 'The person who reads too much and uses his brain too little will fall into lazy habits of thinking',
+    source: 'Albert Einstein',
+    field: '(Scientist)'
   },
   {
-    quote: '"It is our choices, that show what we truly are, far more than our abilities"',
-    source: "J.K. Rowling",
-    citation: "Harry Potter and the Chamber of Secrets",
-    year: "1998",
-    field: "Author"
+    quote: 'It is our choices, that show what we truly are, far more than our abilities',
+    source: 'J.K. Rowling',
+    citation: 'Harry Potter and the Chamber of Secrets',
+    year: '1998',
+    field: '(Author)'
   },
   {
-    quote: '"If a person has no dreams, they no longer have any reason to live. Dreaming is necessary, although in the dream reality should be glimpsed. For me this is the principle of life."',
-    source: "Ayrton Senna",
-    field: "Sportsman"
+    quote: 'If a person has no dreams, they no longer have any reason to live. Dreaming is necessary, although in the dream reality should be glimpsed. For me this is the principle of life.',
+    source: 'Ayrton Senna',
+    field: '(Sportsman)'
   },
   {
-    quote: '"It seems to me that the natural world is the greatest source of excitement; the greatest source of visual beauty; the greatest source of intellectual interest. It is the greatest source of so much in life that makes life worth living"',
-    source: "David Attenborough",
-    year: "2000"
+    quote: 'It seems to me that the natural world is the greatest source of excitement; the greatest source of visual beauty; the greatest source of intellectual interest. It is the greatest source of so much in life that makes life worth living',
+    source: 'David Attenborough',
+    year: '2000'
   }
 ];
 
@@ -53,6 +53,9 @@ function printQuote() {
     }
     if (key === 'year') {
       html += '<span class="year">' + randomQuote.year + '</span>'
+    }
+    if (key === 'field') {
+      html += '<span class="field">' + randomQuote.field + '</span>'
     }
   }
   html += '</p>';
